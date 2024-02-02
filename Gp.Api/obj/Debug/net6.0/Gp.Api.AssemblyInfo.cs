1dc2b00d5c9f1399adc1b715151377d93893cb6b
Jilan-Hisham-Khashaba <jilanhisham02@gmail.com>
2024-02-02 02:37:09 +0200

    i added test folder

--- Gp.Api/obj/Debug/net6.0/Gp.Api.AssemblyInfo.cs
+++ Gp.Api/obj/Debug/net6.0/Gp.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdd5b83b1420fb23a2bb7c088e1028e334bb7a53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
